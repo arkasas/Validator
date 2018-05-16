7d11b4ecdfa6896fbf0f3758dd731019525dac26
Arkadiusz Pituła <apitula@corelogic.pl>
2018-05-16 10:44:21 +0200

    Initial Commit

---
+++ Validator/Validator.h
@@ -0,0 +1,19 @@
+//
+//  Validator.h
+//  Validator
+//
+//  Created by Arkadiusz Pituła on 16.05.2018.
+//  Copyright © 2018 Arkadiusz Pituła. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Validator.
+FOUNDATION_EXPORT double ValidatorVersionNumber;
+
+//! Project version string for Validator.
+FOUNDATION_EXPORT const unsigned char ValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Validator/PublicHeader.h>
+
+
