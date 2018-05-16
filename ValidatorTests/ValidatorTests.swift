4e942ce9b4a2d1207825131d7c879ce30e50346b
Arkadiusz Pituła <apitula@corelogic.pl>
2018-05-16 13:18:38 +0200

    Add tests

--- ValidatorTests/ValidatorTests.swift
+++ ValidatorTests/ValidatorTests.swift
@@ -10,7 +10,8 @@ import XCTest
 @testable import Validator
 
 class ValidatorTests: XCTestCase {
-    
+
+    let validator = Validator()
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,16 +22,85 @@ class ValidatorTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testValidator() {
+        XCTAssertNotNil(validator)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testEmailRegex() {
+        XCTAssertFalse(validator.validate("", regex: EmailRegex()).result)
+        XCTAssertFalse(validator.validate("fjfjdjosdffjidsofsdijfgsdojfgdjso", regex: EmailRegex()).result)
+        XCTAssertFalse(validator.validate("11111111111", regex: EmailRegex()).result)
+        XCTAssertFalse(validator.validate("//////////////////", regex: EmailRegex()).result)
+        XCTAssertTrue(validator.validate("test@test.test", regex: EmailRegex()).result)
     }
-    
+
+    func testStrongPasswordRegex() {
+        XCTAssertFalse(validator.validate("", regex: StrongPasswordRegex()).result)
+        XCTAssertFalse(validator.validate("fjfjdjosdffjidsofsdijfgsdojfgdjso", regex: StrongPasswordRegex()).result)
+        XCTAssertFalse(validator.validate("11111111111", regex: StrongPasswordRegex()).result)
+        XCTAssertFalse(validator.validate("//////////////////", regex: StrongPasswordRegex()).result)
+        XCTAssertFalse(validator.validate("test@test.test", regex: StrongPasswordRegex()).result)
+        XCTAssertTrue(validator.validate("Zaq12345@", regex: StrongPasswordRegex()).result)
+    }
+
+    func testPhoneRegex() {
+        XCTAssertFalse(validator.validate("", regex: PhoneRegex()).result)
+        XCTAssertFalse(validator.validate("fjfjdjosdffjidsofsdijfgsdojfgdjso", regex: PhoneRegex()).result)
+        XCTAssertFalse(validator.validate("//////////////////", regex: PhoneRegex()).result)
+        XCTAssertFalse(validator.validate("test@test.test", regex: PhoneRegex()).result)
+        XCTAssertTrue(validator.validate("1234567890", regex: PhoneRegex()).result)
+        XCTAssertTrue(validator.validate("12345678901234", regex: PhoneRegex()).result)
+    }
+
+    func testLetterWithoutSpaceRegex() {
+        XCTAssertFalse(validator.validate("", regex: LetterRegexWithoutSpaceRegex()).result)
+        XCTAssertTrue(validator.validate("fjfjdjosdffjidsofsdijfgsdojfgdjso", regex: LetterRegexWithoutSpaceRegex()).result)
+        XCTAssertFalse(validator.validate("//////////////////", regex: LetterRegexWithoutSpaceRegex()).result)
+        XCTAssertFalse(validator.validate("test@test.test", regex: LetterRegexWithoutSpaceRegex()).result)
+        XCTAssertFalse(validator.validate("1234567890", regex: LetterRegexWithoutSpaceRegex()).result)
+        XCTAssertFalse(validator.validate("12345678901234", regex: LetterRegexWithoutSpaceRegex()).result)
+        XCTAssertTrue(validator.validate("test test", regex: LetterRegexWithoutSpaceRegex()).result)
+        XCTAssertTrue(validator.validate(" ", regex: LetterRegexWithoutSpaceRegex()).result)
+    }
+
+    func testAlphabeticStringFirstLetterCapsRegex() {
+        XCTAssertFalse(validator.validate("", regex: AlphabeticStringFirstLetterCapsRegex()).result)
+        XCTAssertFalse(validator.validate("fjfjdjosdffjidsofsdijfgsdojfgdjso", regex: AlphabeticStringFirstLetterCapsRegex()).result)
+        XCTAssertFalse(validator.validate("//////////////////", regex: AlphabeticStringFirstLetterCapsRegex()).result)
+        XCTAssertFalse(validator.validate("test@test.test", regex: AlphabeticStringFirstLetterCapsRegex()).result)
+        XCTAssertFalse(validator.validate("1234567890", regex: AlphabeticStringFirstLetterCapsRegex()).result)
+        XCTAssertTrue(validator.validate("Test", regex: AlphabeticStringFirstLetterCapsRegex()).result)
+    }
+
+    func testAlphabeticStringWithSpaceRegex() {
+        XCTAssertTrue(validator.validate("", regex: AlphabeticStringWithSpaceRegex()).result)
+        XCTAssertTrue(validator.validate("fjfjdjosdffidsofsdijfgsdojfgdjso", regex: AlphabeticStringWithSpaceRegex()).result)
+        XCTAssertFalse(validator.validate("//////////////////", regex: AlphabeticStringWithSpaceRegex()).result)
+        XCTAssertFalse(validator.validate("test@test.test", regex: AlphabeticStringWithSpaceRegex()).result)
+        XCTAssertFalse(validator.validate("1234567890", regex: AlphabeticStringWithSpaceRegex()).result)
+        XCTAssertTrue(validator.validate("Test", regex: AlphabeticStringWithSpaceRegex()).result)
+        XCTAssertTrue(validator.validate("Test Tesrt", regex: AlphabeticStringWithSpaceRegex()).result)
+    }
+
+    func testPostCodeRegex() {
+        XCTAssertFalse(validator.validate("", regex: PostCodeRegex()).result)
+        XCTAssertFalse(validator.validate("fjfjdjosdffjidsofsdijfgsdojfgdjso", regex: PostCodeRegex()).result)
+        XCTAssertFalse(validator.validate("//////////////////", regex: PostCodeRegex()).result)
+        XCTAssertFalse(validator.validate("test@test.test", regex: PostCodeRegex()).result)
+        XCTAssertFalse(validator.validate("1234567890", regex: PostCodeRegex()).result)
+        XCTAssertFalse(validator.validate("22-22", regex: PostCodeRegex()).result)
+        XCTAssertTrue(validator.validate("22-212", regex: PostCodeRegex()).result)
+    }
+
+    func testCreditCardRegex() {
+        XCTAssertFalse(validator.validate("", regex: CreditCardRegex()).result)
+        XCTAssertFalse(validator.validate("fjfjdjosdffjidsofsdijfgsdojfgdjso", regex: CreditCardRegex()).result)
+        XCTAssertFalse(validator.validate("//////////////////", regex: CreditCardRegex()).result)
+        XCTAssertFalse(validator.validate("test@test.test", regex: CreditCardRegex()).result)
+        XCTAssertFalse(validator.validate("1234567890", regex: CreditCardRegex()).result)
+        XCTAssertFalse(validator.validate("22-212", regex: CreditCardRegex()).result)
+        XCTAssertFalse(validator.validate("111", regex: CreditCardRegex()).result)
+        XCTAssertTrue(validator.validate("111111111111", regex: CreditCardRegex()).result)
+        XCTAssertTrue(validator.validate("1111111111111111111", regex: CreditCardRegex()).result)
+    }
 }
